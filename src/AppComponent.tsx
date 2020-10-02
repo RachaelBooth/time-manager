f3174c9cdab5aa41b90bfaed49def39a961d9f74
Rachael Booth <rachael.booth@softwire.com>
2020-10-02 22:28:53 +0100

    Add react

---
+++ src/AppComponent.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+const App = () => <div>I'm a component</div>
+
+/*class App extends React.Component {
+    render(): ReactElement {
+        return (
+            <div>I'm a component</div>
+        )
+    }
+}*/
+
+export default App;
